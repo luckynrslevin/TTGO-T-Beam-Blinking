4c8c1cff5525e0b3ccc6de6e2dae14ee4e201892
luckynrslevin <luckynrslevin@users.noreply.github.com>
2020-06-07 15:32:43 +0200

    Add power on functionality

--- src/TTGO-T-Beam-Blinking.ino
+++ src/TTGO-T-Beam-Blinking.ino
@@ -1,12 +1,9 @@
 #include <Wire.h>
-#include "axp20x.h"
+#include <axp20x.h>
 
-#define I2C_SDA             21
-#define I2C_SCL             22
-
-#ifndef AXP192_SLAVE_ADDRESS
-#define AXP192_SLAVE_ADDRESS    0x34
-#endif
+#define I2C_SDA 21
+#define I2C_SCL 22
+#define AXP192_SLAVE_ADDRESS 0x34
 
 AXP20X_Class axp;
 
@@ -45,30 +42,36 @@ void scanI2Cdevice(void)
 }
 
 void setup() {
-
   Serial.begin(115200);
-
   Wire.begin(I2C_SDA, I2C_SCL);
-  
   scanI2Cdevice();
-  
     if (axp192_found) {
         if (!axp.begin(Wire, AXP192_SLAVE_ADDRESS)) {
             Serial.println("AXP192 Begin PASS");
+            // power on device
+            //axp.setPowerOutPut(AXP192_LDO2, AXP202_ON);
+            //axp.setPowerOutPut(AXP192_LDO3, AXP202_ON);
+            //axp.setPowerOutPut(AXP192_DCDC1, AXP202_ON);
+            //axp.setPowerOutPut(AXP192_DCDC2, AXP202_ON);
+            //axp.setPowerOutPut(AXP192_DCDC3, AXP202_ON);
+            //axp.setPowerOutPut(AXP192_EXTEN, AXP202_ON);
+            // this one seems to be enough to make the led work.
+            axp.setDCDC1Voltage(3300); //Set Pin header 3.3V line to 3.3V. Processor is happy down to 1.8V which saves power
+            
+            // Set mode of blue onboard LED (OFF, ON, Blinking 1Hz, Blinking 4 Hz)
+            // axp.setChgLEDMode(AXP20X_LED_OFF);
+            //axp.setChgLEDMode(AXP20X_LED_LOW_LEVEL);
+            axp.setChgLEDMode(AXP20X_LED_BLINK_1HZ);
+            //axp.setChgLEDMode(AXP20X_LED_BLINK_4HZ);
         } else {
             Serial.println("AXP192 Begin FAIL");
         }
-        // Set mode of blue onboard LED (OFF, ON, Blinking 1Hz, Blinking 4 Hz)
-        // axp.setChgLEDMode(AXP20X_LED_OFF);
-        //axp.setChgLEDMode(AXP20X_LED_LOW_LEVEL);
-        axp.setChgLEDMode(AXP20X_LED_BLINK_1HZ);
-        //axp.setChgLEDMode(AXP20X_LED_BLINK_4HZ);
     } else {
         Serial.println("AXP192 not found");
     }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // put your main code here, to run repeatedly
+  // not required in our case.
 }
